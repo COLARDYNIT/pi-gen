aec4d3207509ec40f001b638d53db27c8f5e4db6
sthaes <st.haesendonck@gmail.com>
2017-05-23 15:14:45 +0200

    changed permissions for docker shell script

--- stage2/01-sys-tweaks/01-run.sh
+++ stage2/01-sys-tweaks/01-run.sh
@@ -2,7 +2,7 @@
 mkdir ${ROOTFS_DIR}/home/pi/app
 mkdir ${ROOTFS_DIR}/home/pi/app/logocontrol
 mkdir ${ROOTFS_DIR}/home/pi/app/mysql
-install -m 644 -c files/docker.sh   			${ROOTFS_DIR}/home/pi/app/
+install -m 755 -c files/docker.sh   			${ROOTFS_DIR}/home/pi/app/
 install -m 644 files/regenerate_ssh_host_keys.service	${ROOTFS_DIR}/lib/systemd/system/
 install -m 755 files/apply_noobs_os_config		${ROOTFS_DIR}/etc/init.d/
 install -m 755 files/resize2fs_once			${ROOTFS_DIR}/etc/init.d/
