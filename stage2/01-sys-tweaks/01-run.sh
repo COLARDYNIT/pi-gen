6f6202db9a18e59eca74bd365872598affda33f0
sthaes <st.haesendonck@gmail.com>
2017-05-29 10:25:46 +0200

    see if java installs

--- stage2/01-sys-tweaks/01-run.sh
+++ stage2/01-sys-tweaks/01-run.sh
@@ -3,7 +3,7 @@ mkdir ${ROOTFS_DIR}/home/pi/app
 mkdir ${ROOTFS_DIR}/home/pi/app/logocontrol
 mkdir ${ROOTFS_DIR}/home/pi/app/mysql
 install -m 755 -c files/docker.sh   			${ROOTFS_DIR}/home/pi/app/
-install -m 755 -c files/logopihipsterv-2-0.0.1-SNAPSHOT.war   			${ROOTFS_DIR}/home/pi/app/
+#install -m 755 -c files/logopihipsterv-2-0.0.1-SNAPSHOT.war   			${ROOTFS_DIR}/home/pi/app/
 install -m 644 files/regenerate_ssh_host_keys.service	${ROOTFS_DIR}/lib/systemd/system/
 install -m 755 files/apply_noobs_os_config		${ROOTFS_DIR}/etc/init.d/
 install -m 755 files/resize2fs_once			${ROOTFS_DIR}/etc/init.d/
