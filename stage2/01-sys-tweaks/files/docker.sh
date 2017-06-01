0661e972e5efa11c66cc22bab9de5c5cd9efe5a8
sthaes <st.haesendonck@gmail.com>
2017-06-01 15:02:00 +0200

    changed order of moving files

--- stage2/01-sys-tweaks/files/docker.sh
+++ stage2/01-sys-tweaks/files/docker.sh
@@ -4,7 +4,7 @@ sleep 2m
 echo starting docker
 echo making sure no duplicate containers are running
 docker rm -f $(docker ps -a -q)
-docker run --restart=always --net=host -p 3306:3306 -v /home/pi/app/mysql:/var/lib/mysql -e MYSQL_USER=root -e MYSQL_ALLOW_EMPTY_PASSWORD=yes -e MYSQL_DATABASE=${APP_NAME} -d mitchese/rpi3-mariadb
+docker run --restart=always --net=host -p 3306:3306 -v /home/pi/app/mysql:/var/lib/mysql -e MYSQL_USER=root -e MYSQL_ALLOW_EMPTY_PASSWORD=yes -e MYSQL_DATABASE=fullstackdev_2017B -d mitchese/rpi3-mariadb
 echo started docker
 echo starting app
 java -jar /home/pi/app/*.war
