8c0dc823fdc4181ae4462b27ade39f808e550bce
sthaes <st.haesendonck@gmail.com>
2017-06-02 16:59:44 +0200

    config

--- stage2/01-sys-tweaks/files/docker.sh
+++ stage2/01-sys-tweaks/files/docker.sh
@@ -4,7 +4,7 @@ sleep 2m
 echo starting docker
 echo making sure no duplicate containers are running
 docker rm -f $(docker ps -a -q)
-docker run --restart=always --net=host -p 3306:3306 -v /home/pi/app/mysql:/var/lib/mysql -e MYSQL_USER=root -e MYSQL_PASSWORD=root -e MYSQL_DATABASE=fullstackdev_2017B -d mitchese/rpi3-mariadb
+docker run --restart=always --net=host -p 3306:3306 -v /home/pi/app/mysql:/var/lib/mysql -e MYSQL_USER=root -e MYSQL_ROOT_PASSWORD=root -e MYSQL_DATABASE=fullstackdev_2017B -d mitchese/rpi3-mariadb
 echo started docker
 echo starting app
 sudo java -jar /home/pi/app/*.war
