b8752df8e0eedf58859e6bf53c26f190c47d3331
sthaes <st.haesendonck@gmail.com>
2017-06-07 11:18:01 +0200

    java log file & wait

--- stage2/01-sys-tweaks/files/docker.sh
+++ stage2/01-sys-tweaks/files/docker.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-sleep 2m
 echo starting docker
 echo making sure no duplicate containers are running
 docker rm -f $(docker ps -a -q)
-docker run --restart=always --net=host -p 3306:3306 -v /home/pi/app/mysql:/var/lib/mysql -e MYSQL_USER=root -e MYSQL_ROOT_PASSWORD=root -e MYSQL_DATABASE=fullstackdev_2017B -d mitchese/rpi3-mariadb
+docker run --restart=always --net=host -p 3306:3306 -v /home/pi/app/mysql:/var/lib/mysql -e MYSQL_USER=root -e MYSQL_ROOT_PASSWORD=root -e MYSQL_DATABASE=fullstackdev_2017B -d mitchese/rpi3-mariadb & PID=$!
 echo started docker
+wait $PID
 echo starting app
-sudo java -jar /home/pi/app/*.war
+sudo java -jar /home/pi/app/*.war &> /var/logs/fullstackdev_2017B.log
 echo started app
