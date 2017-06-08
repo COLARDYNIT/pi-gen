d9b0128bd013a7537087193daacc1c7a41e86e56
sthaes <st.haesendonck@gmail.com>
2017-06-08 13:51:56 +0200

    moved chown to on_chroot

--- stage2/01-sys-tweaks/01-run.sh
+++ stage2/01-sys-tweaks/01-run.sh
@@ -7,7 +7,6 @@ install -m 755 -c files/dbsetup.sh   			${ROOTFS_DIR}/home/pi/app/
 install -m 755 -c files/java.sh   			${ROOTFS_DIR}/home/pi/app/
 touch ${ROOTFS_DIR}/home/pi/app/logs/app.log
 mv files/*.war   			${ROOTFS_DIR}/home/pi/app/
-sudo chown -R pi:pi /home/pi
 install -m 644 files/regenerate_ssh_host_keys.service	${ROOTFS_DIR}/lib/systemd/system/
 install -m 755 files/apply_noobs_os_config		${ROOTFS_DIR}/etc/init.d/
 install -m 755 files/resize2fs_once			${ROOTFS_DIR}/etc/init.d/
@@ -46,6 +45,7 @@ EOF
 
 on_chroot << EOF
 usermod --pass='*' root
+sudo chown -R pi:pi /home/pi
 curl -sSL https://get.docker.com | sh
 yes | sudo apt-get update
 yes | sudo apt-get install oracle-java8-jdk
