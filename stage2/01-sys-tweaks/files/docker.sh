7a7ecd0cab5a23ec93d08cc04bf011ff03d046c3
sthaes <st.haesendonck@gmail.com>
2017-06-08 16:47:56 +0200

    added path to dbsetup script

--- stage2/01-sys-tweaks/files/docker.sh
+++ stage2/01-sys-tweaks/files/docker.sh
@@ -6,5 +6,5 @@ NAME="database"
 docker run --name=$NAME --restart=always --net=host -p 3306:3306 -v /home/pi/app/mysql:/var/lib/mysql -e MYSQL_USER=root -e MYSQL_ROOT_PASSWORD=root  -d mitchese/rpi3-mariadb & PID=$!
 echo started docker
 wait $PID
-sudo docker exec -i $NAME /bin/bash < dbsetup.sh
+sudo docker exec -i $NAME /bin/bash < /home/pi/app/dbsetup.sh
 
