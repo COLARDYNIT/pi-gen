8acc6926d78bfc3dd5987eab6a6aa39fe1f55cd0
sthaes <st.haesendonck@gmail.com>
2017-06-09 15:25:24 +0200

    added path to dbsetup script

--- stage2/01-sys-tweaks/files/dbsetup.sh
+++ stage2/01-sys-tweaks/files/dbsetup.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-sudo mysql -u root -proot -e "CREATE USER 'app'@'localhost' IDENTIFIED BY 'apppwd';GRANT ALL ON *.* TO 'app'@'localhost';CREATE DATABASE IF NOT EXISTS fullstackdev_2017B;"
+sudo mysql -u root -proot -e "CREATE USER 'app'@'localhost' IDENTIFIED BY 'apppwd';GRANT ALL ON *.* TO 'app'@'localhost';CREATE DATABASE IF NOT EXISTS fullstackdev_2017C;"
